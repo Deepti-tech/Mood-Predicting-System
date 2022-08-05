ce08a4efe2c4ca3b1b0c155fec338e5a7cae27c2
Deepti-tech <80256433+Deepti-tech@users.noreply.github.com>
2022-08-05 19:25:31 +0530

    Create Help.py

---
+++ Mood-Predicting-System.py/Help.py
@@ -0,0 +1,6 @@
+def more_about_us():
+    print("\nThis mood predicting system will help you in predicting your mood, we can suggest you some tips to enhance your mood and track your mood if you want to check it in the future.")
+    print("\n\nHere, we have four moods, which are 'Happy', 'Sad', 'Worried' and 'Excited'")
+    print("\n\nIf you want to know your mood, press '1'. \nYou will be put through some questions.\nKindly answer them in Yes/No format only\nBased on your answers, we will predict your mood.")
+    print("\n\nNow, that you know your mood but are confused what to do next, then press '2'. \nThen enter your mood.\nFor example : Press 'Happy' if you are happy.\nAnd, you will get suggestions which will help you get in better mood.")
+    print("\n\nDo feel like you experience a lot of mood swings and unfortunately are not sure about it.\nDo not worry!!\nJust press '3'. \nNow, you have the history of the past days since day 1, so you can be sure about your mood swings.")
